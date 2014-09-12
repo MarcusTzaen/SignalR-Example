303d4192fa0d2c940880d52a04377a24730690f6
MarcusTzaen <marcus_0230@163.com>
2014-09-12 21:03:23 +0800

    init

---
+++ OwinStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Timers;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(WebApplication1.OwinStartup))]
+
+namespace WebApplication1
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+            
+        }
+    }
+}
---
+++ PhotoHub.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Timers;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class PhotoHub : Hub
+    {
+        public void Init(string path)
+        {
+            // Call the broadcastMessage method to update clients.
+            //Clients.All.broadcastMessage(name, message);
+            PathPool.Add(GetBinary(Directory.GetFiles(path).FirstOrDefault()));
+            var timer = new Timer(3000);
+            timer.Elapsed += (s, e) =>
+            {
+                GlobalHost.ConnectionManager.GetHubContext<PhotoHub>().Clients.All.SetImg(PathPool.Lst.FirstOrDefault());
+            };
+            timer.Enabled = true;
+        }
+
+
+        private string GetBinary(string fileName)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var bytes = File.ReadAllBytes(fileName);
+                return "data:image/gif;base64," + Convert.ToBase64String(bytes);
+            }
+        }
+    }
+
+    public static class PathPool
+    {
+        private static List<string> _lst;
+        public static List<string> Lst
+        {
+            get { return _lst; }
+        }
+        public static void Add(string path)
+        {
+            if (_lst == null)
+            {
+                _lst = new List<string>();
+            }
+            _lst.Add(path);
+        }
+    }
+}
